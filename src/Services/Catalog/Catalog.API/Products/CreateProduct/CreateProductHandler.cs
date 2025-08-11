17f43d7b74f10eb3bd1811e73284ab93aa5d425b
SangNguyenHome <sangnt.developer@gmail.com>
2025-08-11 22:35:16 +0700

    - Added ICommand, ICommandHandler, IQuery, IQueryHandler classes. - Added Product model to Catalog.API - Added CreateProductEndpoint, CreateProductHandler for Catalog.API - Added Docker Compose default.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+public class CreateProductHandler(IDocumentSession session)
+    : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = request.Name,
+            Category = request.Category,
+            Description = request.Description,
+            ImageFile = request.ImageFile,
+            Price = request.Price,
+        };
+
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog([typeof(Program).Assembly]));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
 
 app.Run();
