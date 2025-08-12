4726593bdaf90f08f71d036e6495171633ce8e94
SangNguyenHome <sangnt.developer@gmail.com>
2025-08-12 22:04:06 +0700

    - Added postgres image to docker compose - Started postgres server with docker compose - Added new endpoint for GetProducts

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsRequest();
+
+public record GetProductsResponse(IEnumerable<Product> products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var resposne = result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(resposne);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<Product> products);
+
+public class GetProductsHandler(IDocumentSession session, ILogger<GetProductsHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>().ToListAsync();
+        return new GetProductsResult(products);
+    }
+}
