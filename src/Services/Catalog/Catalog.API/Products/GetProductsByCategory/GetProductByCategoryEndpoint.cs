f82b0f97ed962369ed4c10e692750aee5034b0fa
SangNguyenHome <sangnt.developer@gmail.com>
2025-08-27 23:32:45 +0700

    Added endpoints and handlers for Delete, GetProductById, GetProductByCategory, UpdateProduct.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+//public record GetProductByCategoryRequest();
+
+public record GetProductByCategoryResponse(Product Product);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsByCategoryQuery(category));
+
+            var response = result.Adapt<GetProductsByCategoryResult>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Category")
+        .WithDescription("Get Product By Category");
+    }
+}
