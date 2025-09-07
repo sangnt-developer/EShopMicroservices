0bb9172f2228ca61df78b64ba9d9c2918852e352
SangNguyenHome <sangnt.developer@gmail.com>
2025-09-07 21:20:10 +0700

    - Created `Basket.API` project - Created models as ``ShoppingCart`, `ShoppingCartItem` classes. - Created empty handlers and endpoints for Delete/Get/Store basket. - Added reference to BuildingBlock project. - Added `GlobalUsing` to declare global for `using`'s - Configured `launchSettings` to run debug basket api with http on 5001 port, https on 5051 port.

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+// public record GetBasketRequest(string Username);
+
+public record GetBasketResponse(ShoppingCart cart);
+
+public class GetBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(userName));
+
+            var response = result.Adapt<GetBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetBasketByUsername")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Basket By Username")
+        .WithDescription("Get Basket By Username");
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+    {
+        // TODO: Get basket from database
+        // var basket = await _repository.GetBasket(request.UserName);
+
+        return new GetBasketResult(new ShoppingCart("swn"));
+    }
+}
