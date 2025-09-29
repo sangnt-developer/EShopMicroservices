a0a11bbf6ca11e7496daf18be5dba6cf781d88d6
SangNguyenHome <sangnt.developer@gmail.com>
2025-09-29 21:57:35 +0700

    - Renamed Ordering.API and its folder. - Added dependency injection class to layers of clean architecture on Ordering.API project. - Registered services on each layers to Ordering.API layer. - Added abstraction classes follow DDD as Entity, Aggregate, DomainEvent, etc. - Added domain models as Order, OrderItem, Product, Customer, etc. - Added value objects as Address, Payment records. - Added value objects as CustomerId, OrderId, OrderItemId, OrderName records to prevent `primitive obsession`.

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.ValueObjects;
+
+public record OrderName
+{
+    public string Value { get; set; }
+}
