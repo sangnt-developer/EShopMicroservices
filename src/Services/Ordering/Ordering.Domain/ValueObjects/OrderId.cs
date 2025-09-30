d2bb77e540f834446d000c698f9334f24d9d9279
SangNguyenHome <sangnt.developer@gmail.com>
2025-09-30 21:50:38 +0700

    - Implemented Of() static method on value objects - Implemented Create() static method on domain models - Added domain logic/behaviors to Order domain model as Create, Update, Add, Remove. - Implemented 2 domain events OrderCreatedEvent, OrderUpdatedEvent. - Installed `Microsoft.EntityFrameworkCore.SqlServer`, `Microsoft.EntityFrameworkCore.Tools` packages to Ordering.Infrastructure project.

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -3,4 +3,15 @@
 public record OrderId
 {
     public Guid Value { get; }
+    private OrderId(Guid value) => Value = value;
+    public static OrderId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("OrderId can not be empty");
+        }
+
+        return new OrderId(value);
+    }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
@@ -3,4 +3,15 @@
 public record OrderItemId
 {
     public Guid Value { get; }
+    private OrderItemId(Guid value) => Value = value;
+    public static OrderItemId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("OrderItemId can not be empty");
+        }
+
+        return new OrderItemId(value);
+    }
 }
